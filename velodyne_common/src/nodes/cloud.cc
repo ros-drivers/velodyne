8a350c4af48e6f6bc4477a1133767e8b77a3f53d
jack.oquin <jack.oquin@f2b30410-025d-11df-9f77-c71dee008b03>
2010-11-09 04:58:35 +0000

    Clean up stand-alone point cloud node. Remove deprecated launch file. Make visualization launch files using nodelets.

--- velodyne_common/src/nodes/cloud.cc
+++ velodyne_common/src/nodes/cloud.cc
@@ -17,7 +17,6 @@
 #include <sensor_msgs/PointCloud.h>
 
 #include <velodyne/data.h>
-#include <velodyne/running_stats.hh>
 
 #define NODE "velodyne_cloud"
 
@@ -27,46 +26,10 @@ using namespace velodyne_common;
 static int qDepth = 1;                  // ROS topic queue size
 
 // local static data
-static RunningStats latency;
-static uint32_t droppedMsgs = 0;
 static velodyne::DataXYZ *data = NULL;
 static ros::Publisher output;
 
 sensor_msgs::PointCloud pc;             // outgoing PointCloud message
-
-/** \brief accumulate and log message latency, dropped messages */
-void checkLatency(const roslib::Header *hdr)
-{
-  static bool firstMsg = true;          // have we seen any yet?
-  static uint32_t nextSeq = 0;          // next sequence number expected
-
-  // accumulate latency mean and variance
-  double timeDelay = ros::Time::now().toSec() - hdr->stamp.toSec();
-  latency.addSample(timeDelay);
-  ROS_DEBUG("message %u received, latency: %.3f seconds",
-            hdr->seq, timeDelay);
-
-  // check for lost messages
-  if (firstMsg)
-    {
-      firstMsg = false;
-    }
-  else if (nextSeq < hdr->seq)
-    {
-      // If the publisher terminates and then restarts, the sequence
-      // numbers apparently reset.  We don't count that as a dropped
-      // message.
-      uint32_t missed = hdr->seq - nextSeq;
-      if (droppedMsgs == 0)
-        // only log INFO message the first time
-        ROS_INFO("%u Velodyne messages dropped", missed);
-      else
-        // log DEBUG message for any remaining losses
-        ROS_DEBUG("%u more Velodyne messages dropped", missed);
-      droppedMsgs += missed;
-    }
-  nextSeq = hdr->seq + 1;
-}
 
 
 /** \brief callback for XYZ points
@@ -76,7 +39,6 @@ void checkLatency(const roslib::Header *hdr)
 void processXYZ(const std::vector<velodyne::laserscan_xyz_t> &scan)
 {
   const roslib::Header *hdr = data->getMsgHeader();
-  checkLatency(hdr);
 
   // pass along original time stamp and frame ID
   pc.header.stamp = hdr->stamp;
@@ -148,7 +110,7 @@ int getParameters(int argc, char *argv[])
 
   data = new velodyne::DataXYZ();
   data->getParams();
-  data->subscribeXYZ(processXYZ);
+  data->subscribe(boost::bind(&processXYZ, _1));
 
   return 0;
 }
@@ -181,17 +143,11 @@ int main(int argc, char *argv[])
   pc.channels[0].name = "intensity";
   pc.channels[0].values.resize(velodyne::SCANS_PER_REV);
 
-  ROS_INFO(NODE ": starting main loop");
+  ROS_DEBUG(NODE ": starting main loop");
 
   ros::spin();                          // handle incoming data
 
-  ROS_INFO(NODE ": exiting main loop");
-  ROS_INFO("message latency: %.3f +/- %.3f",
-           latency.getMean(), latency.getStandardDeviation());
-  if (droppedMsgs)
-    ROS_INFO("total of %u Velodyne messages dropped", droppedMsgs);
-  else
-    ROS_INFO("no Velodyne messages dropped");
+  ROS_DEBUG(NODE ": exiting main loop");
 
   data->shutdown();
   delete data;
