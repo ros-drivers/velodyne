8e6ba44bbf44b6b3cfb0b5316e87396dfe1cb539
piyushk <piyushk@f2b30410-025d-11df-9f77-c71dee008b03>
2011-10-03 19:30:46 +0000

    some pretty printing + fixed subscriber to do euclidean cluster extraction using PCL

--- velodyne_pcl/examples/nodes/subscriber.cc
+++ velodyne_pcl/examples/nodes/subscriber.cc
@@ -5,14 +5,56 @@
 
 #include <boost/foreach.hpp>
 
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/kdtree/tree_types.h>
+#include <pcl/kdtree/impl/tree_types.hpp>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/kdtree/impl/organized_data.hpp>
+#include <pcl/segmentation/impl/extract_clusters.hpp>
+
 typedef velodyne_pcl::PointXYZIR VPoint;
 typedef pcl::PointCloud<VPoint> VPointCloud;
 
 void callback(const VPointCloud::ConstPtr& msg) {
-  printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-  //BOOST_FOREACH (const VPoint& pt, msg->points) {
-    //printf ("\t(%f, %f, %f, %f, %i)\n", pt.x, pt.y, pt.z, pt.intensity, pt.ring);
-  //}
+  ROS_INFO("Cloud: width = %d, height = %d", msg->width, msg->height);
+
+  // Extracting clusters.
+  pcl::KdTree<VPoint>::Ptr tree (new pcl::KdTreeFLANN<VPoint>);
+  tree->setInputCloud (msg);
+
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<VPoint> ec;
+  ec.setClusterTolerance(0.1); 
+  ec.setMinClusterSize(100);
+  ec.setMaxClusterSize(200);
+  ec.setSearchMethod(tree);
+  ec.setInputCloud(msg);
+  ec.extract(cluster_indices);
+
+  int numClusters = cluster_indices.size();
+  printf("\tNum Clusters: %i\n", numClusters);
+
+  BOOST_FOREACH(const pcl::PointIndices &clusterIndex, cluster_indices) {
+    velodyne_pcl::PointXYZIR point;
+    long ring = 0;
+    point.x = point.y = point.z = point.intensity = point.ring = 0;
+    for (unsigned int j = 0; j < clusterIndex.indices.size(); j++) {
+      point.x += msg->points[clusterIndex.indices[j]].x;
+      point.y += msg->points[clusterIndex.indices[j]].y;
+      point.z += msg->points[clusterIndex.indices[j]].z;
+      point.intensity += msg->points[clusterIndex.indices[j]].intensity;
+      ring += msg->points[clusterIndex.indices[j]].ring;
+    }
+    point.intensity /= clusterIndex.indices.size();
+    ring /= clusterIndex.indices.size();
+    point.z /= clusterIndex.indices.size();
+    point.x /= clusterIndex.indices.size();
+    point.y /= clusterIndex.indices.size();
+    
+    printf ("\t\t(%f, %f, %f, %f, %li)\n", point.x, point.y, point.z, point.intensity, ring);
+    
+  }
+  
 }
 
 int main(int argc, char** argv) {
