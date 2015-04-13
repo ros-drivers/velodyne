dff8003b4bc6c2f7887fc6b61ccfff7004d15a6b
Jit Ray Chowdhury <jit.ray.c@gmail.com>
2015-04-13 21:14:32 +0530

    converted tab to space

--- velodyne_driver/src/lib/input.cc
+++ velodyne_driver/src/lib/input.cc
@@ -142,16 +142,16 @@ namespace velodyne_driver
         ssize_t nbytes = recvfrom(sockfd_, &pkt->data[0],
                                   packet_size,  0, NULL, NULL);
 
-	if (nbytes < 0)
-	  {
+    if (nbytes < 0)
+      {
             if (errno != EWOULDBLOCK)
- 	      {
+          {
                 perror("recvfail");
-		ROS_INFO("recvfail");
+        ROS_INFO("recvfail");
                 return 1;
-	      }
-	  }
-	else if ((size_t) nbytes == packet_size)
+          }
+      }
+    else if ((size_t) nbytes == packet_size)
           {
             // read successful, done now
             break;
@@ -242,20 +242,20 @@ namespace velodyne_driver
               packet_rate_.sleep();
             if((header)->len - 42 == 1206)
             {
-				memcpy(&pkt->data[0], pkt_data+42, packet_size);
-				pkt->stamp = ros::Time::now();
-				
-			}else if((header)->len-42 == 512)
-			{
-				ROS_INFO_ONCE("Position data ignored ");
+                memcpy(&pkt->data[0], pkt_data+42, packet_size);
+                pkt->stamp = ros::Time::now();
+
+            }else if((header)->len-42 == 512)
+            {
+                ROS_INFO_ONCE("Position data ignored ");
                 return 1;
-			}else
-			{
-				ROS_WARN("Incorrect data length %d (1206 LIDAR data, 512 position data): ",
+            }else
+            {
+                ROS_WARN("Incorrect data length %d (1206 LIDAR data, 512 position data): ",
                      (header)->len-42);
                 return 1;
-			}
-			empty_ = false;
+            }
+            empty_ = false;
             return 0;                   // success
           }
 
