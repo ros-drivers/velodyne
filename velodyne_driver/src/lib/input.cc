43d7807e72db337a7cf35c2cfef508643f5a31cd
Jit Ray Chowdhury <jit.ray.c@gmail.com>
2015-04-13 21:07:46 +0530

    Added filter to ignore position data from Velodyne 32E while reading pcap file. Otherwise position data was read as a part of lidar data causing a gap in th point cloud in hte correct RPM.

--- velodyne_driver/src/lib/input.cc
+++ velodyne_driver/src/lib/input.cc
@@ -240,10 +240,22 @@ namespace velodyne_driver
             // Keep the reader from blowing through the file.
             if (read_fast_ == false)
               packet_rate_.sleep();
-            
-            memcpy(&pkt->data[0], pkt_data+42, packet_size);
-            pkt->stamp = ros::Time::now();
-            empty_ = false;
+            if((header)->len - 42 == 1206)
+            {
+				memcpy(&pkt->data[0], pkt_data+42, packet_size);
+				pkt->stamp = ros::Time::now();
+				
+			}else if((header)->len-42 == 512)
+			{
+				ROS_INFO_ONCE("Position data ignored ");
+                return 1;
+			}else
+			{
+				ROS_WARN("Incorrect data length %d (1206 LIDAR data, 512 position data): ",
+                     (header)->len-42);
+                return 1;
+			}
+			empty_ = false;
             return 0;                   // success
           }
 
