1760f81112ed61ea6a412fea602befd19112732a
CNR <cn.ritter@fu-berlin.de>
2022-11-07 13:27:32 +0100

    add Velodyne VLS-128 support to velodyne_pointcloud based on ROS 1 master branch on tag 1.7.0

--- velodyne_pointcloud/include/velodyne_pointcloud/rawdata.hpp
+++ velodyne_pointcloud/include/velodyne_pointcloud/rawdata.hpp
@@ -36,6 +36,8 @@
 #include <pcl/point_cloud.h>
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/clock.hpp>
+
 #include <velodyne_msgs/msg/velodyne_packet.hpp>
 
 #include <memory>
@@ -98,6 +100,22 @@ static const int BLOCKS_PER_PACKET = 12;
 static const int PACKET_STATUS_SIZE = 4;
 static const int SCANS_PER_PACKET = (SCANS_PER_BLOCK * BLOCKS_PER_PACKET);
 
+/** Special Definitions for VLS128 support **/
+// These are used to detect which bank of 32 lasers is in this block
+static const uint16_t VLS128_BANK_1 = 0xeeff;
+static const uint16_t VLS128_BANK_2 = 0xddff;
+static const uint16_t VLS128_BANK_3 = 0xccff;
+static const uint16_t VLS128_BANK_4 = 0xbbff;
+
+// [µs] Channels corresponds to one laser firing
+static const float VLS128_CHANNEL_TDURATION = 2.665f;
+// [µs] Sequence is a set of laser firings including recharging
+static const float VLS128_SEQ_TDURATION = 53.3f;
+// [µs] μs. Top Of the Hour is aligned with the fourth firing group in a firing sequence.
+static const float VLS128_TOH_ADJUSTMENT = 8.7f;
+static const float VLS128_DISTANCE_RESOLUTION = 0.004f;  // [m]
+static const float VLS128_MODEL_ID = 161;
+
 /** \brief Raw Velodyne packet.
  *
  *  revolution is described in the device manual as incrementing
@@ -121,7 +139,30 @@ struct raw_packet
 class RawData final
 {
 public:
+  // RawData();
   RawData(const std::string & calibration_file, const std::string & model);
+
+  ~RawData()
+  {
+  }
+
+  void setupSinCosCache();
+  void setupAzimuthCache();
+
+  /** \brief Set up for data processing offline.
+   * Performs the same initialization as in setup, in the abscence of a ros::NodeHandle.
+   * this method is useful if unpacking data directly from bag files, without passing
+   * through a communication overhead.
+   *
+   * @param calibration_file path to the calibration file
+   * @param model sensor model type
+   * @param max_range_ cutoff for maximum range
+   * @param min_range_ cutoff for minimum range
+   * @returns 0 if successful;
+   *           errno value for failure
+   */
+  // int setupOffline(std::string calibration_file, std::string model, double max_range_,
+  //                  double min_range_);
 
   void unpack(
     const velodyne_msgs::msg::VelodynePacket & pkt, DataContainerBase & data,
@@ -135,15 +176,20 @@ public:
 
 private:
   /** configuration parameters */
-  struct Config
+  typedef struct
   {
     std::string model;
-    double min_range;             ///< minimum range to publish
+    std::string calibrationFile;  ///< calibration file name
     double max_range;             ///< maximum range to publish
+    double min_range;             ///< minimum range to publish
     int min_angle;                ///< minimum angle to publish
     int max_angle;                ///< maximum angle to publish
-  };
-  Config config_{};
+
+    double tmp_min_angle;
+    double tmp_max_angle;
+  }
+  Config;
+  Config config_;
 
   /**
    * Calibration file
@@ -151,6 +197,9 @@ private:
   std::unique_ptr<velodyne_pointcloud::Calibration> calibration_;
   float sin_rot_table_[ROTATION_MAX_UNITS]{};
   float cos_rot_table_[ROTATION_MAX_UNITS]{};
+
+  // Caches the azimuth percent offset for the VLS-128 laser firings
+  float vls_128_laser_azimuth_cache[16];
 
   // timing offset lookup table
   std::vector<std::vector<float>> timing_offsets_;
@@ -167,8 +216,17 @@ private:
   void unpack_vlp16(
     const velodyne_msgs::msg::VelodynePacket & pkt, DataContainerBase & data,
     const rclcpp::Time & scan_start_time);
+
+  void unpack_vls128(
+    const velodyne_msgs::msg::VelodynePacket & pkt, DataContainerBase & data,
+    const rclcpp::Time & scan_start_time);
+
+  /** in-line test whether a point is in range */
+  inline bool pointInRange(float range)
+  {
+    return range >= config_.min_range && range <= config_.max_range;
+  }
 };
 
 }  // namespace velodyne_rawdata
-
 #endif  // VELODYNE_POINTCLOUD__RAWDATA_HPP_
