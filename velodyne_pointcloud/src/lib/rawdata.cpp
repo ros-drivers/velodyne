1893a0e39793275a83b6f18c4734bdd216181173
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2023-05-29 17:41:00 +0900

    feat: support vls128 for ros2 (#493)
    * Update rolling ci (#512)
    
    Fix Rolling CI and associated linter errors.
    
    * Add support for the velodyne Alpha Prime (#370)
    
    * Add support for the velodyne Alpha Prime
    
    * Change packet rate for the VLS128 according to the times specified in the manual
    
    * Organize setup functions to avoid code duplication. Add a constant for the model ID of the VLS128.
    
    * Use the defined constants to calculate the time offset of the points for the VLS128
    
    Co-authored-by: jugo <juan.gonzalez@unibw.de>
    
    * Add VLS128 launch and calibration file (#382)
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * vls128: add line only once all four banks are processed (#413)
    
    Signed-off-by: Sebastian Scherer (CR/AAS3) <sebastian.scherer2@de.bosch.com>
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * fix: apply uncrustify
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Fixing incorrect type in velodyne_pointcloud.
    
    * Fixed non working vls128 fork
    
    * Added organized cloud compliance and remove some useless code
    
    * Corrected the azimuth offset calculation and put the declation of variable where there are used
    
    * Fix linter errors.
    
    ---------
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    Signed-off-by: Sebastian Scherer (CR/AAS3) <sebastian.scherer2@de.bosch.com>
    Co-authored-by: Joshua Whitley <josh@electrifiedautonomy.com>
    Co-authored-by: Institute for Autonomous Systems Technology <tas@unibw.de>
    Co-authored-by: jugo <juan.gonzalez@unibw.de>
    Co-authored-by: Sebastian Scherer <sebastian.scherer2@de.bosch.com>
    Co-authored-by: wep21 <border_goldenmarket@yahoo.co.jp>
    Co-authored-by: Joshua Whitley <josh.whitley@autoware.org>
    Co-authored-by: Mtestor <mucevval@gmail.com>

--- velodyne_pointcloud/src/lib/rawdata.cpp
+++ velodyne_pointcloud/src/lib/rawdata.cpp
@@ -35,6 +35,9 @@
 #include <cmath>
 #include <memory>
 #include <string>
+#include <cassert>
+
+#include <velodyne_msgs/msg/velodyne_packet.hpp>
 
 #include <velodyne_msgs/msg/velodyne_packet.hpp>
 
@@ -63,12 +66,8 @@ RawData::RawData(const std::string & calibration_file, const std::string & model
 
   // RCLCPP_INFO(this->get_logger(), "Number of lasers: %d.",  calibration_->num_lasers);
 
-  // Set up cached values for sin and cos of all the possible headings
-  for (uint16_t rot_index = 0; rot_index < ROTATION_MAX_UNITS; ++rot_index) {
-    float rotation = angles::from_degrees(ROTATION_RESOLUTION * rot_index);
-    cos_rot_table_[rot_index] = ::cosf(rotation);
-    sin_rot_table_[rot_index] = ::sinf(rotation);
-  }
+  setupSinCosCache();
+  setupAzimuthCache();
 }
 
 /** Update parameters: conversions and update */
@@ -188,6 +187,29 @@ bool RawData::buildTimings()
           (single_firing * dataPointIndex);
       }
     }
+  } else if (config_.model == "VLS128") {
+    timing_offsets_.resize(3);
+    for (size_t i = 0; i < timing_offsets_.size(); ++i) {
+      timing_offsets_[i].resize(17);  // 17 (+1 for the maintenance time after firing group 8)
+    }
+
+    double full_firing_cycle = VLS128_SEQ_TDURATION * 1e-6;   // seconds
+    double single_firing = VLS128_CHANNEL_TDURATION * 1e-6;   // seconds
+    double offset_paket_time = VLS128_TOH_ADJUSTMENT * 1e-6;  // seconds
+    double sequenceIndex, firingGroupIndex;
+    // Compute timing offsets
+    for (size_t x = 0; x < timing_offsets_.size(); ++x) {
+      for (size_t y = 0; y < timing_offsets_[x].size(); ++y) {
+        sequenceIndex = x;
+        firingGroupIndex = y;
+        timing_offsets_[x][y] =
+          (full_firing_cycle * sequenceIndex) + (single_firing * firingGroupIndex) -
+          offset_paket_time;
+        RCLCPP_DEBUG(
+          rclcpp::get_logger("velodyne_pointcloud"),
+          "firing_seque %lu firing_group %lu offset %f", x, y, timing_offsets_[x][y]);
+      }
+    }
   } else {
     timing_offsets_.clear();
     RCLCPP_WARN(
@@ -219,6 +241,30 @@ int RawData::numLasers() const
   return calibration_->num_lasers;
 }
 
+void RawData::setupSinCosCache()
+{
+  // Set up cached values for sin and cos of all the possible headings
+  for (uint16_t rot_index = 0; rot_index < ROTATION_MAX_UNITS; ++rot_index) {
+    float rotation = angles::from_degrees(ROTATION_RESOLUTION * rot_index);
+    cos_rot_table_[rot_index] = ::cosf(rotation);
+    sin_rot_table_[rot_index] = ::sinf(rotation);
+  }
+}
+
+void RawData::setupAzimuthCache()
+{
+  if (config_.model == "VLS128") {
+    for (uint8_t i = 0; i < 16; i++) {
+      vls_128_laser_azimuth_cache_[i] =
+        (VLS128_CHANNEL_TDURATION / VLS128_SEQ_TDURATION) * (i + i / 8);
+    }
+  } else {
+    RCLCPP_WARN(
+      rclcpp::get_logger("velodyne_pointcloud"),
+      "No Azimuth Cache configured for model %s", config_.model.c_str());
+  }
+}
+
 /** @brief convert raw packet to point cloud
  *
  *  @param pkt raw packet to unpack
@@ -229,6 +275,12 @@ void RawData::unpack(
   const rclcpp::Time & scan_start_time)
 {
   // RCLCPP_DEBUG(this->get_logger(), "Received packet, time: %s", pkt.stamp);
+
+  /** special parsing for the VLS128 **/
+  if (pkt.data[1205] == VLS128_MODEL_ID) {  // VLS 128
+    unpack_vls128(pkt, data, scan_start_time);
+    return;
+  }
 
   /** special parsing for the VLP16 **/
   if (calibration_->num_lasers == 16) {
@@ -276,6 +328,7 @@ void RawData::unpack(
         (block.rotation <= config_.max_angle ||
         block.rotation >= config_.min_angle)))
       {
+        float time = 0;
         if (timing_offsets_.size()) {
           time = timing_offsets_[i][j] + time_diff_start_to_this_packet;
         }
@@ -400,6 +453,179 @@ void RawData::unpack(
     }
 
     data.newLine();
+  }
+}
+
+/** @brief convert raw VLS128 packet to point cloud
+ *
+ *  @param pkt raw packet to unpack
+ *  @param pc shared pointer to point cloud (points are appended)
+ */
+void RawData::unpack_vls128(
+  const velodyne_msgs::msg::VelodynePacket & pkt, DataContainerBase & data,
+  const rclcpp::Time & scan_start_time)
+{
+  const raw_packet * raw = reinterpret_cast<const raw_packet *>(&pkt.data[0]);
+
+  bool dual_return = (pkt.data[1204] == 57);
+  for (int block = 0; block < BLOCKS_PER_PACKET - (4 * dual_return); block++) {
+    // cache block for use
+    const raw_block & current_block = raw->blocks[block];
+
+    int bank_origin = 0;
+    // Used to detect which bank of 32 lasers is in this block
+    switch (current_block.header) {
+      case VLS128_BANK_1:
+        bank_origin = 0;
+        break;
+      case VLS128_BANK_2:
+        bank_origin = 32;
+        break;
+      case VLS128_BANK_3:
+        bank_origin = 64;
+        break;
+      case VLS128_BANK_4:
+        bank_origin = 96;
+        break;
+      default:
+        // ignore packets with mangled or otherwise different contents
+        // Do not flood the log with messages, only issue at most one
+        // of these warnings per minute.
+        auto clock = rclcpp::Clock{};
+        RCLCPP_WARN_STREAM_THROTTLE(
+          rclcpp::get_logger("velodyne_pointcloud"), clock, 60000,
+          "skipping invalid VLS-128 packet: block " <<
+            block << " header value is " <<
+            raw->blocks[block].header);
+        return;  // bad packet: skip the rest
+    }
+
+    uint16_t azimuth{};        // Uniform initializion. equivalent to azimuth = 0;
+    uint16_t azimuth_next{};
+    float azimuth_diff{};
+    // Calculate difference between current and next block's azimuth angle.
+    if (!dual_return) {
+      switch (block) {
+        // 1 firing sequence span across 4 data block, causing them to have the same azimuth
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+          azimuth = raw->blocks[0].rotation;
+          azimuth_next = raw->blocks[4].rotation;
+          break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        // We assume that the difference between 3rd firing sequence and the next packet firing
+        // sequence is the same as difference between 2nd and 3rd firing sequence.
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+          azimuth = raw->blocks[4].rotation;
+          azimuth_next = raw->blocks[8].rotation;
+          break;
+        default:
+          assert(false && "block should be between 0 and BLOCK_PER_PACKET(12)");
+          break;
+      }
+      azimuth_diff = static_cast<float>((36000 + azimuth_next - azimuth) % 36000);
+    } else {
+      // We can't calculate for dual return, since it have 1 azimuth per data packet and
+      // we don't have access to the next packet.
+      azimuth = raw->blocks[0].rotation;
+    }
+
+    for (int j = 0, k = 0; j < SCANS_PER_BLOCK; j++, k += RAW_SCAN_SIZE) {
+      // Offset the laser in this block by which block it's in
+      uint8_t laser_number = j + bank_origin;
+      // VLS-128 fires 8 lasers at a time
+      uint8_t firing_order = laser_number / 8;
+
+      float time_diff_start_to_this_packet = (rclcpp::Time(pkt.stamp) - scan_start_time).seconds();
+      float time {};
+      if (timing_offsets_.size()) {
+        time = timing_offsets_[block / 4][firing_order + laser_number / 64] +
+          time_diff_start_to_this_packet;
+      }
+
+      velodyne_pointcloud::LaserCorrection & corrections =
+        calibration_->laser_corrections[laser_number];
+
+      // distance extraction
+      union two_bytes tmp;
+      tmp.bytes[0] = current_block.data[k];
+      tmp.bytes[1] = current_block.data[k + 1];
+      float distance = tmp.uint * VLS128_DISTANCE_RESOLUTION;
+
+      // correct for the laser rotation as a function of timing during the firings
+      float azimuth_corrected_f = azimuth +
+        (azimuth_diff * vls_128_laser_azimuth_cache_[firing_order]);
+      uint16_t azimuth_corrected = ((uint16_t) round(azimuth_corrected_f)) % 36000;
+
+      // condition added to avoid calculating points
+      // which are not in the interesting defined area (min_angle < area < max_angle)
+      // ((Confusing ! Are min_angle and max_angle included or not ?))
+      if (((config_.min_angle < config_.max_angle &&
+        azimuth_corrected >= config_.min_angle &&
+        azimuth_corrected <= config_.max_angle) ||
+        (config_.min_angle > config_.max_angle)) &&
+        pointInRange(distance))
+      {
+        // convert polar coordinates to Euclidean XYZ
+        // Each laser has an vertical offset and
+        // an horizontal offset in addition to azimut
+        float cos_vert_angle = corrections.cos_vert_correction;
+        float sin_vert_angle = corrections.sin_vert_correction;
+        float cos_rot_correction = corrections.cos_rot_correction;
+        float sin_rot_correction = corrections.sin_rot_correction;
+
+        // cos(a-b) = cos(a)*cos(b) + sin(a)*sin(b)
+        // sin(a-b) = sin(a)*cos(b) - cos(a)*sin(b)
+        float cos_rot_angle =
+          cos_rot_table_[azimuth_corrected] * cos_rot_correction +
+          sin_rot_table_[azimuth_corrected] * sin_rot_correction;
+        float sin_rot_angle =
+          sin_rot_table_[azimuth_corrected] * cos_rot_correction -
+          cos_rot_table_[azimuth_corrected] * sin_rot_correction;
+
+        // (TODO) Improve calculation.
+        // We can see different calculation in the implementation of other sensors.
+        // This implementation is in agreement (if no error was made) with VLS128 manuel,
+        // But it might not be the case with physics.
+
+        // The velodyne VLS-128 manual user show this calcultations.
+        float x = distance * cos_vert_angle * sin_rot_angle;
+        float y = distance * cos_vert_angle * cos_rot_angle;
+        float z = distance * sin_vert_angle;
+
+        /** Use standard ROS coordinate system (right-hand rule) */
+        float x_coord = y;
+        float y_coord = -x;
+        float z_coord = z;
+
+        data.addPoint(
+          x_coord,
+          y_coord,
+          z_coord,
+          corrections.laser_ring,
+          // azimuth_corrected,
+          distance,
+          current_block.data[k + 2],  // intensity
+          time);
+      } else {
+        // call to addPoint is still required since output could be organized
+        data.addPoint(
+          nanf(""), nanf(""), nanf(""), corrections.laser_ring, nanf(""), nanf(""), time);
+      }
+    }
+
+    if (current_block.header == VLS128_BANK_4) {
+      // add a new line only after the last bank (VLS128_BANK_4)
+      data.newLine();
+    }
   }
 }
 
@@ -600,5 +826,4 @@ void RawData::unpack_vlp16(
     }
   }
 }
-
 }  // namespace velodyne_rawdata
